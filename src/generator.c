c2dc39a9bb759081f9b8faaacbb09dd641949d76
majchrzw <wojjmaj22@gmail.com>
2022-04-10 20:56:06 +0200

    zmiany

--- src/generator.c
+++ src/generator.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 #include "generator.h"
 
-void cut_graph(int x, int y,double **edges_tab_x, double **edges_tab_y, int debug_flag)
+void cut_graph(int x, int y,short int **tab_x, short int **tab_y, int debug_flag)
 {
 		int w1, w2;
  		int true = 0;
@@ -56,12 +55,12 @@ void cut_graph(int x, int y,double **edges_tab_x, double **edges_tab_y, int debu
 
 		int vertex = w1; // usuwanie krawędzi na drodze między wylosowanymi wierzchołkami
 		int tmp = 0; // ruch w pionie lub poziomie
-		edges_tab_x[vertex / x][vertex % x] = -1;
-		edges_tab_y[vertex / x][vertex % x] = -1;
+		tab_x[vertex / x][vertex % x] = -1;
+		tab_y[vertex / x][vertex % x] = -1;
 		do
 		{
-			edges_tab_x[vertex / x][vertex % x] = -1;
-			edges_tab_y[vertex / x][vertex % x] = -1;
+			tab_x[vertex / x][vertex % x] = -1;
+		    tab_y[vertex / x][vertex % x] = -1;
 			if (tmp % 2 == 0)
 			{
 				if (vertex % x > w2 % x)
@@ -86,64 +85,65 @@ void cut_graph(int x, int y,double **edges_tab_x, double **edges_tab_y, int debu
 			}
 			tmp++;
 		} while (vertex != w2);
-		edges_tab_x[vertex / x][vertex % x] = -1;
-		edges_tab_y[vertex / x][vertex % x] = -1;
+		tab_x[vertex / x][vertex % x] = -1;
+		tab_y[vertex / x][vertex % x] = -1;
 }
 
 void create_graph(int x, int y, char *plik, double range_begin, double range_end, int amount, int debug_flag)
 {
-	printf("Tworzenie grafu o wymiarach %ix%i, dzielonego %i-razy, z wagami krawędzi w zakresie <%.2lf;%.2lf> i zapis do pliku %s.\n", x, y, amount, range_begin, range_end, plik);
 
-	srand(time(NULL));
+    printf("Tworzenie grafu o wymiarach %ix%i, dzielonego %i-razy, z wagami krawędzi w zakresie <%.2lf;%.2lf> i zapis do pliku %s.\n", x, y, amount, range_begin, range_end, plik);
+    FILE *out = fopen( plik, "w");
+    srand(time(NULL));
+    int counter = 0;
+   
+    if(amount == 0)
+    {
+        fprintf( out, "%i %i\n", x, y);
+        while ( counter < x*y)
+        {
+            fprintf( out, "%i - ", counter);
+            if( counter % x != x-1)
+                fprintf( out, "%i :%lf  ", counter + 1, range_begin + ((rand() / (double)RAND_MAX) * (range_end - range_begin)));
+            if( counter < (x*y - x))
+                fprintf( out, "%i :%lf  ", counter + x, range_begin + ((rand() / (double)RAND_MAX) * (range_end - range_begin)));
+            fprintf(out, "\n");
+            counter++;
+        }
 
-	int counter = 0;  // licznik wierzchołków
-	int counter2 = 0; // drugi licznik
+        fclose(out);
+        exit(0);
+    }
 
-	int wiersze_x = y, wiersze_y = y;
+    int wiersze_x = y, wiersze_y = y;
 	int kolumny_x = x, kolumny_y = x;
 
-	double **edges_tab_x = malloc ( wiersze_x * sizeof(double*));
-	double **edges_tab_y = malloc ( wiersze_y * sizeof(double*));
-
+    short int **tab_x = malloc ( wiersze_x * sizeof(short int*));
 	for( int i = 0; i < wiersze_x; i++)
 	{
-		edges_tab_x[i] = malloc( kolumny_x * sizeof(double));
+		tab_x[i] = malloc( kolumny_x * sizeof(short int));
+        for( int j = 0; j < kolumny_x; j++)
+        {
+            tab_x[i][j] = 0;
+        }
 	}
+
+    short int **tab_y = malloc ( wiersze_y * sizeof(short int*));
 	for( int i = 0; i < wiersze_y; i++)
 	{
-		edges_tab_y[i] = malloc( kolumny_y * sizeof(double));
+		tab_y[i] = malloc( kolumny_y * sizeof(short int));
+        for( int j = 0; j < kolumny_y; j++)
+        {
+            tab_y[i][j] = 0;
+        }
 	}
 
-	for (int i = 0; i < wiersze_x; i++) // generowanie krawędzi poziomych
-	{
-		for (int j = 0; j < kolumny_x; j++)
-		{
-			edges_tab_x[i][j] = range_begin + ((rand() / (double)RAND_MAX) * (range_end - range_begin));
-		}
-	}
+    for(int i = 0; i < amount; i++)
+    {
+        cut_graph( x, y, tab_x, tab_y, debug_flag);
+    }
 
-	for (int i = 0; i < wiersze_y; i++) // generowanie krawędzi pionowych
-	{
-		for (int j = 0; j < kolumny_y; j++)
-		{
-			edges_tab_y[i][j] = range_begin + ((rand() / (double)RAND_MAX) * (range_end - range_begin));
-		}
-	}
-
-	FILE *out = fopen(plik, "w"); // plik do zapisu danych
-
-	if (out == NULL) // test czy plik jest ok
-	{
-		fprintf(stderr, "Nie znalazłem lub nie moge otworzyć pliku do odczytu.\n");
-		exit(1);
-	}
-
-	for (int i = 0; i < amount; i++) // cięcie grafu określoną ilość razy
-	{
-		cut_graph( x, y, edges_tab_x, edges_tab_y, debug_flag);
-	}
-
-	if (debug_flag == 1) // Drukowanie do zobaczenia podziału grafu na stdout - funkcja do debugownaia głównie
+    if (debug_flag == 1) // Drukowanie do zobaczenia podziału grafu na stdout - funkcja do debugownaia głównie
 	{
 		int tmp2 = 0;
 		for (int i = 0; i < 2*y-1; i++)
@@ -153,7 +153,7 @@ void create_graph(int x, int y, char *plik, double range_begin, double range_end
 				for (int j = 0; j < x; j++)
 				{
 					printf("*");
-					if (j != x - 1 && edges_tab_x[ i/2][ j] != -1)
+					if (j != x - 1 && tab_x[ i/2][ j] != -1)
 						printf("--");
 					else
 						printf("  ");
@@ -164,7 +164,7 @@ void create_graph(int x, int y, char *plik, double range_begin, double range_end
 			{
 				for (int j = 0; j < x; j++)
 				{
-					if (edges_tab_y[ i/2][ j] != -1)
+					if (tab_y[ i/2][ j] != -1)
 						printf("|  ");
 					else
 						printf("   ");
@@ -175,31 +175,28 @@ void create_graph(int x, int y, char *plik, double range_begin, double range_end
 		}
 	}
 
-	fprintf(out, "%i %i\n", x, y);
-	for (int i = 0; i < x; i++)
+    fprintf( out, "%i %i\n", x, y);
+        while ( counter < x*y)
+        {
+            fprintf( out, "%i - ", counter);
+            if( counter % x != x-1)
+                if(tab_x[counter/x][counter%x] != -1)
+                    fprintf( out, "%i :%lf  ", counter + 1, range_begin + ((rand() / (double)RAND_MAX) * (range_end - range_begin)));
+            if( counter < (x*y - x))
+                if(tab_y[counter/x][counter%x] != -1)
+                    fprintf( out, "%i :%lf  ", counter + x, range_begin + ((rand() / (double)RAND_MAX) * (range_end - range_begin)));
+            fprintf(out, "\n");
+            counter++;
+        }
+    for ( int i = 0; i < wiersze_x; i++)
 	{
-		for (int j = 0; j < y; j++)
-		{
-			if (j != y - 1)
-				if (edges_tab_x[counter / x][counter2] != -1)
-					fprintf(out, "%i :%lf  ", counter + 1, edges_tab_x[counter / x][counter2]); // połączenie w prawo
-			if (i != x - 1)
-				if (edges_tab_y[counter / x][counter2] != -1)
-					fprintf(out, "%i :%lf", counter + x, edges_tab_y[counter / x][counter2]); // połączenie w dół
-			counter++;
-			fprintf(out, "\n");
-		}
-		counter2++;
+		free(tab_x[i]);
 	}
-	for ( int i = 0; i < y; i++)
+	for ( int i = 0; i < kolumny_y; i++)
 	{
-		free(edges_tab_x[i]);
+		free(tab_y[i]);
 	}
-	for ( int i = 0; i < y; i++)
-	{
-		free(edges_tab_y[i]);
-	}
-	fclose(out);
-	free(edges_tab_x);
-	free(edges_tab_y);
+    free(tab_x);
+    free(tab_y);
+    fclose(out);
 }
